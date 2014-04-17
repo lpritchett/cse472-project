ce62b621fd0fbfa1f47f67a73b2f04f518132ea2
lpritchett <pritch93@gmail.com>
2014-04-17 18:38:22 -0400

    first committ

---
+++ BouncingCube/BouncingCube/ChildView.h
@@ -0,0 +1,49 @@
+
+// ChildView.h : interface of the CChildView class
+//
+
+
+#pragma once
+#include "ShaderWnd/ShaderWnd.h"
+#include "Cube.h"
+#include "GrTexture.h"
+// CChildView window
+
+class CChildView : public CShaderWnd
+{
+// Construction
+public:
+	CChildView();
+
+// Attributes
+public:
+	CCube *m_cube, *m_wall;
+	bool m_bTimer;
+	int m_nTimer;
+	float m_fT;
+
+// Operations
+public:
+
+// Overrides
+	protected:
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+
+// Implementation
+public:
+	virtual ~CChildView();
+	virtual void InitGL();
+	virtual void RenderGL();
+	virtual void CleanGL();
+
+	// Generated message map functions
+protected:
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags);
+	afx_msg void OnTimer(UINT_PTR nIDEvent);
+private:
+	CGrTexture m_brick;
+	
+};
+
